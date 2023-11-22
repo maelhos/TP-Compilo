7d9ed556126b3c93e7f3de049b4120b2a7c5c66c
Augustin Perrin <augustinperrin@MacBook-Pro-dAugustin.local>
2023-11-22 15:23:53 +0100

    fichier avec type et exemples

---
+++ ast.ml
@@ -0,0 +1,52 @@
+type ast =
+        | Imm of int (* valeur immediate *)
+        | Arg of int (* reference au nieme argument *)
+        | Add of (ast * ast) (* addition des deux sous-arbres *)
+        | Sub of (ast * ast) (* soustraction *)
+        | Mul of (ast * ast) (* multiplication *)
+        | Div of (ast * ast) (* division *);;
+
+(* exemples : *)
+(* 
+   ast de [ x y ] ( x + y ) / 2 
+*)
+let ast1 = Div(Add(Arg 0, Arg 1), Imm 2);;
+
+(* 
+   ast de [ x ] x + 2*5 
+*)
+let ast2 = Add(Arg 0, Mul(Imm 2, Imm 5));;
+
+let tokenize code =
+        (* Entree :
+            Une chaine de caracteres representant une fonction dans le langage.
+           Sortie :
+            Une liste de tokens associes au code.
+           *);;
+
+
+exception CompilerError of string;;
+
+let pass1 code =
+        (* Entree :
+            Une chaine de caracteres representant une fonction dans le langage.
+           Sortie :
+            L'AST associe a la fonction. 
+        *)
+        raise (CompilerError "pass1 : todo");;
+
+let pass2 (tree:ast) -> ast =
+        (* Entree :
+            L'AST du premier pass.
+           Sortie :
+            L'AST simplifie selon la specification du TP.
+           *)
+        raise (CompilerError "pass2 : todo");;
+
+let pass3 (tree:ast) -> string list =
+        (* Entree :
+            L'AST simplifie du second pass.
+           Sortie :
+            Une liste de directives assembleur.
+           *)
+        raise (CompilerError "pass3 : todo");;
