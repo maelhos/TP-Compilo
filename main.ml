621124a6f9d4c6b27a4b1bfdfcf51da0373d6eb0
maelhos <52194580+maelhos@users.noreply.github.com>
2023-11-22 20:43:58 +0100

    test and corriged

--- main.ml
+++ main.ml
@@ -1,2 +1,4 @@
 
-print_float 1.0;;
+Token.print_tokenlist (Token.lex (Token.explode "[ xaxa, y ] xaxa + y * (xaxa - y)"));;
+print_newline ();;
+Token.print_tokenlist (Token.lex (Token.explode "[ x ] x * 9"));;
--- token.ml
+++ token.ml
@@ -7,84 +7,79 @@ type token =
 
 
 let isnumber = function
-        |'0' .. '9' -> true
-        |_ -> false;;
+  | '0' .. '9' -> true
+  | _ -> false;;
 
 let islowercasealpha = function
-        |'a' .. 'z' -> true
-        |_ -> false;;
+  | 'a' .. 'z' -> true
+  | _ -> false;;
 
 (* --- Le lexer --- *)
 let rec explode code =
-        (* Entree :
-            Une chaine de caracteres representant une fonction dans le langage.
-           Sortie :
-            Le tableau de caracteres associe a la chaine.
-           *)
-        if String.length code = 0 then []
-        else
-                let following = String.sub code 1 ((String.length code) - 1) in
-                [code.[0]] @ explode following;;
+  (* Entree :
+      Une chaine de caracteres representant une fonction dans le langage.
+     Sortie :
+      Le tableau de caracteres associe a la chaine.
+     *)
+  if String.length code = 0 then []
+  else
+    let following = String.sub code 1 ((String.length code) - 1) in
+    [code.[0]] @ explode following;;
 
 let string_of_char = String.make 1;;
 
 let isspecial t = List.mem t [' '; '['; ']'; '('; ')'; '+'; '-'; '*'; '/'; ','];;
 
 let rec tokenize (code : char list) (liste_tokens : token list) : token list =
-        match code with
-        |[] -> EOF::liste_tokens
-        |' '::q -> tokenize q liste_tokens
-        |'['::q -> tokenize q (RBRA :: liste_tokens)
-        |']'::q -> tokenize q (LBRA :: liste_tokens)
-        |'('::q -> tokenize q (LPAR :: liste_tokens)
-        |')'::q -> tokenize q (RPAR :: liste_tokens)
-        |'+'::q -> tokenize q (ADD :: liste_tokens)
-        |'-'::q -> tokenize q (SUB :: liste_tokens)
-        |'*'::q -> tokenize q (MUL :: liste_tokens)
-        |'/'::q -> tokenize q (DIV :: liste_tokens)
-        |','::q -> tokenize q (COMMA :: liste_tokens)
-        |t::q when isnumber t -> lexZ q (string_of_char t) liste_tokens
-        |t::q when islowercasealpha t -> lexW q (string_of_char t) liste_tokens
-        |_ -> failwith "invalid character used"
+  match code with
+  | [] -> EOF::liste_tokens
+  | ' '::q -> tokenize q liste_tokens
+  | '['::q -> tokenize q (RBRA :: liste_tokens)
+  | ']'::q -> tokenize q (LBRA :: liste_tokens)
+  | '('::q -> tokenize q (LPAR :: liste_tokens)
+  | ')'::q -> tokenize q (RPAR :: liste_tokens)
+  | '+'::q -> tokenize q (ADD :: liste_tokens)
+  | '-'::q -> tokenize q (SUB :: liste_tokens)
+  | '*'::q -> tokenize q (MUL :: liste_tokens)
+  | '/'::q -> tokenize q (DIV :: liste_tokens)
+  | ','::q -> tokenize q (COMMA :: liste_tokens)
+  | t::q when isnumber t -> lexZ q (string_of_char t) liste_tokens
+  | t::q when islowercasealpha t -> lexW q (string_of_char t) liste_tokens
+  | _ -> failwith "invalid character used"
 and lexW (code : char list) (token : string) (liste_tokens : token list) : token list =
-        match code with
-        |[] -> EOF::(VAR token)::liste_tokens
-        |t::q when islowercasealpha t -> lexW q (token ^ (string_of_char t)) liste_tokens
-        |t::q when isspecial t -> tokenize code ((VAR token)::liste_tokens)
-        |_::q -> failwith "invalid argument name"
+  match code with
+  | [] -> EOF::(VAR token)::liste_tokens
+  | t::q when islowercasealpha t -> lexW q (token ^ (string_of_char t)) liste_tokens
+  | t::_ when isspecial t -> tokenize code ((VAR token)::liste_tokens)
+  | _ -> failwith "invalid argument name"
 and lexZ (code : char list) (token : string) (liste_tokens : token list) : token list =
-        match code with
-        |[] -> EOF::(CONST (int_of_string token))::liste_tokens
-        |t::q when isnumber t -> lexZ q (token ^ (string_of_char t)) liste_tokens
-        |t::q when isspecial t -> tokenize code ((CONST (int_of_string token))::liste_tokens)
-        |_::q -> failwith "invalid number";;
+  match code with
+  | [] -> EOF::(CONST (int_of_string token))::liste_tokens
+  | t::q when isnumber t -> lexZ q (token ^ (string_of_char t)) liste_tokens
+  | t::_ when isspecial t -> tokenize code ((CONST (int_of_string token))::liste_tokens)
+  | _ -> failwith "invalid number";;
 
 
 let lex (code:char list) : token list =
-        List.rev (tokenize code []);;
+  List.rev (tokenize code []);;
 
 
 
 (* -- fonctions d'affichage -- *)
 let token_to_string (t : token) : string =
-        match t with
-        |ADD -> "+"
-        |SUB -> "-"
-        |MUL -> "*"
-        |DIV -> "/"
-        |LPAR -> "("
-        |RPAR -> ")"
-        |RBRA -> "["
-        |LBRA -> "]"
-        |VAR x -> x
-        |CONST x -> (string_of_int x)
-        |EOF -> ""
-        |COMMA -> ",";;
+  match t with
+  | ADD -> "+"
+  | SUB -> "-"
+  | MUL -> "*"
+  | DIV -> "/"
+  | LPAR -> "("
+  | RPAR -> ")"
+  | RBRA -> "["
+  | LBRA -> "]"
+  | VAR x -> x
+  | CONST x -> (string_of_int x)
+  | EOF -> ""
+  | COMMA -> ",";;
 
 let print_tokenlist (t : token list) : unit =
-        List.iter (fun x -> print_string (token_to_string x)) t;;
-
-
-print_tokenlist (lex (explode "[ xaxa, y ] xaxa + y * (xaxa - y)"));;
-print_newline ();;
-print_tokenlist (lex (explode "[ x ] x * 9"));;
+  List.iter (fun x -> print_string (token_to_string x)) t;;
